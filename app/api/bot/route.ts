cb3faf6433bc40c3be953b4aa5c1537fefac6cf8
rickmoralis <megapowerbob@gmail.com>
2024-09-03 10:24:51 +0300

    edit command

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -9,8 +9,8 @@ const token = process.env.TELEGRAM_BOT_TOKEN
 if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
 
 const bot = new Bot(token)
-bot.on('message:text', async (ctx) => {
-    await ctx.reply(ctx.message.text)
+bot.command('start', async (ctx) => {
+    await ctx.reply('HELLO')
 })
 
 export const POST = webhookCallback(bot, 'std/http')
