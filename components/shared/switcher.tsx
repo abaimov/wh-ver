06fac036846476c21b9e330a56209fd7ca0099b7
rickmoralis <megapowerbob@gmail.com>
2024-09-09 22:01:54 +0300

    fix#13

--- components/shared/switcher.tsx
+++ components/shared/switcher.tsx
@@ -11,7 +11,11 @@ export default function Switcher() {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({message: 'Petuh',users:["550269978"],img:'https://upload.wikimedia.org/wikipedia/ru/b/bb/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80%D0%BD%D0%BE%D0%B9_%D0%B8%D0%B3%D1%80%D1%8B_Cyberpunk_2077.jpg'}),  // Отправляем новое состояние
+            body: JSON.stringify({
+                message: 'Petuh',
+                users: ["550269978"],
+                img: 'https://images.unsplash.com/photo-1445053023192-8d45cb66099d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }),  // Отправляем новое состояние
         });
     };
 
