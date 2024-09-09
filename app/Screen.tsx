c11e4562255d4150415ce976422779d897903027
rickmoralis <megapowerbob@gmail.com>
2024-09-10 00:11:02 +0300

    modify del img

--- app/Screen.tsx
+++ app/Screen.tsx
@@ -2,27 +2,43 @@
 
 import Switcher from "@/components/shared/switcher";
 import FileUploader from "@/components/shared/imageUpload";
+import { useState } from "react";
 
-export default function Screen({ images }: any) {
-    const delImg = async (url: string) => {
+interface Image {
+    url: string;
+    downloadUrl: string;
+    pathname: string;
+    size: number;
+    uploadedAt: Date;
+}
+
+export default function Screen({ images }: { images: Image[] }) {
+    const [dataImages, setDataImages] = useState<Image[]>(images);
+
+    const delImg = async (pathname: string) => {
         try {
             const response = await fetch('/api/delete', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ pathname: url }),
+                body: JSON.stringify({ pathname }),
             });
 
             const result = await response.json();
 
             if (response.ok) {
+                setDataImages(prev => prev.filter(image => image.pathname !== pathname));
                 console.log('IMG DEL', result.message);
             } else {
                 console.error('ERROR DELETE IMG', result.message);
             }
-        } catch (e) {
-            console.error('ERROR DELETE IMG', e);
+        } catch (e: unknown) {
+            let errorMessage = 'Unknown error occurred';
+            if (e instanceof Error) {
+                errorMessage = e.message;
+            }
+            console.error('ERROR DELETE IMG', errorMessage);
         }
     };
 
@@ -30,12 +46,12 @@ export default function Screen({ images }: any) {
         <>
             <Switcher />
             <FileUploader />
-            {images.map((el: any, index: number) => (
+            {dataImages.map((el: Image, index: number) => (
                 <div key={index} className="flex">
                     <img src={el.url} alt={`pic${index}`} />
                     <div
                         className="bg-red-200 h-[30px] w-[100px] text-center cursor-pointer"
-                        onClick={() => delImg(el.url)}
+                        onClick={() => delImg(el.pathname)}
                     >
                         DELETE
                     </div>
