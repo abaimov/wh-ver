0b4c0540718fa1352bfc6ac52822cac01c42e798
rickmoralis <megapowerbob@gmail.com>
2024-09-09 22:12:37 +0300

    fix#16

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,5 @@
-import { Bot } from 'grammy';
-import { NextResponse } from 'next/server';
+import {Bot} from 'grammy';
+import {NextResponse} from 'next/server';
 
 const token = process.env.TELEGRAM_BOT_TOKEN_SECOND;
 if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.');
@@ -14,25 +14,31 @@ export async function POST(req: Request) {
         const body = await req.json();
         console.log('Получено тело запроса:', body);
 
-        const { message, users, img } = body;
+        const {message, users, img} = body;
 
         if (message && Array.isArray(users) && users.length > 0 && img) {
             try {
                 // Отправляем сообщение и изображение каждому пользователю из массива users
                 for (const userId of users) {
-                    // Отправка изображения с текстом
-                    await bot.api.sendPhoto(userId, img, {
-                        caption: message,  // Текстовое сообщение, прикреплённое к изображению
-                    });
+
+                    if (message === 'Petuh') {
+                        await bot.api.sendPhoto(userId, img, {
+                            caption: message,  // Текстовое сообщение, прикреплённое к изображению
+                        });
+                    } else {
+                        await bot.api.sendPhoto(userId, img, {
+                            caption: message,  // Текстовое сообщение, прикреплённое к изображению
+                        });
+                    }
                 }
 
-                return NextResponse.json({ success: true, message: 'Messages with images sent to all users' });
+                return NextResponse.json({success: true, message: 'Messages with images sent to all users'});
             } catch (err) {
                 console.error('Ошибка при отправке сообщений:', err);
-                return NextResponse.json({ success: false, message: 'Error sending messages' });
+                return NextResponse.json({success: false, message: 'Error sending messages'});
             }
         }
     }
 
-    return NextResponse.json({ success: false, message: 'Invalid request' });
+    return NextResponse.json({success: false, message: 'Invalid request'});
 }
--- components/shared/switcher.tsx
+++ components/shared/switcher.tsx
@@ -3,9 +3,7 @@
 import {useState} from 'react';
 
 export default function Switcher() {
-    const [isDeleteMode, setIsDeleteMode] = useState(false);
-
-    const toggleMode = async () => {
+    const toggleMode = async (str: string) => {
         await fetch('/api/test', {
             method: 'POST',
             headers: {
@@ -13,16 +11,19 @@ export default function Switcher() {
             },
             body: JSON.stringify({
                 message: 'Petuh',
-                users: ["550269978","5357256925"],
+                users: ["550269978", "5357256925"],
                 img: 'https://images.unsplash.com/photo-1445053023192-8d45cb66099d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-            }),  // Отправляем новое состояние
+            }),
         });
     };
 
     return (
         <div>
-            <button onClick={toggleMode}>
+            <button onClick={() => toggleMode('Petuh')}>
                 отправить
+            </button>
+            <button onClick={() => toggleMode('TI LOH')}>
+                отправить 2
             </button>
         </div>
     );
